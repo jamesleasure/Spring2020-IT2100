1d2431dfc13c9a9659c178825094462d855b4ef8
James Leasure <james.leasure@tri-c.edu>
2020-01-10 10:40:55 -0500

    Created Lab 1

---
+++ Lab1/Lab1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab1
+//
+//  Created by James Leasure on 1/10/20.
+//  Copyright © 2020 James Leasure. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World from James!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
